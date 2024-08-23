bac6cc550fa43de51d34ccb165b7ce1b8eae2abb
Radu Nanescu <radu.nanescu@gmail.com>
2024-08-23 15:11:43 +0300

    Update greeting.js

--- assets/js/greeting.js
+++ assets/js/greeting.js
@@ -23,12 +23,12 @@ const gree2 = `${CONFIG.greetingMorning}\xa0`;
 const gree3 = `${CONFIG.greetingAfternoon}\xa0`;
 const gree4 = `${CONFIG.greetingEvening}\xa0`;
 
-if (hour >= 23 || hour < 6 && userName) {
-	document.getElementById('greetings').innerText = gree1 + ${userName};
+if ((hour >= 23 || hour < 6) && userName) {
+    document.getElementById('greetings').innerText = `${gree1}${userName}`;
 } else if (hour >= 6 && hour < 12 && userName) {
-	document.getElementById('greetings').innerText = gree2 + ${userName};
+    document.getElementById('greetings').innerText = `${gree2}${userName}`;
 } else if (hour >= 12 && hour < 17 && userName) {
-	document.getElementById('greetings').innerText = gree3 + ${userName};
+    document.getElementById('greetings').innerText = `${gree3}${userName}`;
 } else {
-	document.getElementById('greetings').innerText = gree4 + ${userName};
+    document.getElementById('greetings').innerText = `${gree4}${userName}`;
 }
